db34f372f4a47125f08bb4be0450d72d33eae4f2
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2025-05-02 16:34:35 +0200

    _

--- build.sh
+++ build.sh
@@ -2,18 +2,29 @@
 
 set -eox pipefail
 
-git clone https://github.com/ocaml/ocaml ../ocaml
+sudo apt-get update -y
+sudo apt-get install -y texlive-latex-extra texlive-fonts-recommended texlive-lualatex hevea sass
 
-cd ../ocaml
+git clone https://github.com/ocaml/ocaml
+
+cd ocaml
 
 ./configure --prefix $(pwd)/local
 
-make -j && make -j -C manual web
+make -j
+make -C manual/src/html_processing duniverse
+make -C manual web DISCLAIMER=1
 
 cd -
 
-git checkout --orphan gh-pages
+git checkout -b gh-pages
 
 git rm '*'
 
-cp -R manual/src/webman/*/* .
+cp -R ocaml/manual/src/webman/*/* .
+
+rm -rf ocaml
+
+git add .
+
+git commit -m 'foo'
